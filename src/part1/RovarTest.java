3ae55f53f588572ff628ba5c29dc90413d373e02
FilipHarald <filip.harald@gmail.com>
2016-02-16 15:35:33 +0100

    added some derovartests

--- src/part1/RovarTest.java
+++ src/part1/RovarTest.java
@@ -26,16 +26,37 @@ public class RovarTest {
 
     @Test
     public void testDerovNull() throws Exception {
-
+    	String s = Rovar.derov(null);
+    	assertNull(s);
     }
 
     @Test
     public void testDerovEmpty() throws Exception {
-
+    	String s = Rovar.derov("");
+    	assertEquals("", s);
     }
 
     @Test
-    public void testDerovCorrect() throws Exception {
-
+    public void testDerovCorrectLowerCase() throws Exception {
+    	String s = Rovar.derov("rorovovarorsospoprorakoketot");
+    	assertEquals("rovarspraket", s);
+    }
+    
+    @Test
+    public void testDerovCorrectUpperCase() throws Exception {
+    	String s = Rovar.derov("ROROVOVARORSOSPOPRORAKOKETOT");
+    	assertEquals("ROVARSPRAKET", s);
+    }
+    
+    @Test
+    public void testDerovNumbers() throws Exception {
+    	String s = Rovar.derov("123");
+    	assertEquals("123", s);
+    }
+    
+    @Test
+    public void testDerovOtherChars() throws Exception {
+    	String s = Rovar.derov("!=&");
+    	assertEquals("!=&", s);
     }
 }
