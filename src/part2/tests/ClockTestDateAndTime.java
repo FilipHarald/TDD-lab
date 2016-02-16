34a07e9c3c729ab5db9f12ba0cb89619b7123c56
FilipHarald <filip.harald@gmail.com>
2016-02-16 17:05:15 +0100

    Added syntax-file and finished time-tests

--- src/part2/tests/ClockTestDateAndTime.java
+++ src/part2/tests/ClockTestDateAndTime.java
@@ -1,54 +1,116 @@
 package part2.tests;
 
-import org.junit.Test;
-
 import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import part2.clock.Clock;
 
 /**
  * Created by nekosaur on 2016-02-16.
  */
 public class ClockTestDateAndTime {
-
+	
+	Clock clock;
+	String defaultTime;
+	String defaultDate;
+	
+	@Before
+	public void setUp() {
+		clock = new Clock();
+		defaultDate = clock.changeMode();
+		defaultTime = clock.changeMode();
+	}
+	
     /**
      *
      * Time tests
      *
      */
+	
+	@Test
+	public void testTimeDefault() throws Exception {
+		assertEquals("00:00:00", defaultTime);
+	}
 
     @Test
     public void testTimeSecondLowerBorder() throws Exception {
-
+    	clock.Set(12, 12, -1);
+    	clock.changeMode();
+    	assertEquals(defaultTime, clock.changeMode());
+    	clock.Set(12, 12, 0);
+    	clock.changeMode();
+    	assertEquals("00:00:00", clock.changeMode());
+    	clock.Set(12, 12, 1);
+    	clock.changeMode();
+    	assertEquals("00:00:01", clock.changeMode());
     }
 
     @Test
     public void testTimeSecondUpperBorder() throws Exception {
-
+    	clock.Set(12, 12, 58);
+    	clock.changeMode();
+    	assertEquals("00:00:58", clock.changeMode());
+    	clock.Set(12, 12, 59);
+    	clock.changeMode();
+    	assertEquals("00:00:59", clock.changeMode());
+    	clock.Set(12, 12, 60);
+    	clock.changeMode();
+    	assertEquals(defaultTime, clock.changeMode());
     }
 
     @Test
     public void testTimeMinuteLowerBorder() throws Exception {
-
+    	clock.Set(12, -1, 12);
+    	clock.changeMode();
+    	assertEquals(defaultTime, clock.changeMode());
+    	clock.Set(12, 0, 12);
+    	clock.changeMode();
+    	assertEquals("00:00:00", clock.changeMode());
+    	clock.Set(12, 1, 12);
+    	clock.changeMode();
+    	assertEquals("00:01:00", clock.changeMode());
     }
 
     @Test
     public void testTimeMinuteUpperBorder() throws Exception {
-
+    	clock.Set(12, 58, 12);
+    	clock.changeMode();
+    	assertEquals("00:00:58", clock.changeMode());
+    	clock.Set(12, 59, 12);
+    	clock.changeMode();
+    	assertEquals("00:00:59", clock.changeMode());
+    	clock.Set(12, 60, 12);
+    	clock.changeMode();
+    	assertEquals(defaultTime, clock.changeMode());
     }
 
     @Test
     public void testTimeHourLowerBorder() throws Exception {
-
+    	clock.Set(-1, 12, 12);
+    	clock.changeMode();
+    	assertEquals(defaultTime, clock.changeMode());
+    	clock.Set(0, 12, 12);
+    	clock.changeMode();
+    	assertEquals("00:00:00", clock.changeMode());
+    	clock.Set(1, 12, 12);
+    	clock.changeMode();
+    	assertEquals("01:00:00", clock.changeMode());
     }
 
     @Test
     public void testTimeHourUpperBorder() throws Exception {
-
+    	clock.Set(58, 12, 12);
+    	clock.changeMode();
+    	assertEquals("58:00:00", clock.changeMode());
+    	clock.Set(59, 12, 12);
+    	clock.changeMode();
+    	assertEquals("59:00:00", clock.changeMode());
+    	clock.Set(60, 12, 12);
+    	clock.changeMode();
+    	assertEquals(defaultTime, clock.changeMode());
     }
 
-    @Test
-    public void testTimeDefault() throws Exception {
-
-    }
 
     /**
      *
@@ -56,6 +118,10 @@ public class ClockTestDateAndTime {
      *
      */
 
+    @Test
+    public void testDateDefault() throws Exception {
+    	assertEquals("2000-January-1", defaultDate);
+    }
 
     @Test
     public void testDateYearLowerBorder() throws Exception {
@@ -87,10 +153,4 @@ public class ClockTestDateAndTime {
 
     }
 
-
-
-    @Test
-    public void testDateDefault() throws Exception {
-
-    }
 }
