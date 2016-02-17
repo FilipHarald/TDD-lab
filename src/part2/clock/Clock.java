d49ac59d1638d62e4ac38b24df20174d11ed5e7c
nekosaur <albert@kaaman.nu>
2016-02-17 08:52:12 +0100

    implemented Clock

--- src/part2/clock/Clock.java
+++ src/part2/clock/Clock.java
@@ -2,19 +2,58 @@ package part2.clock;
 
 public class Clock {
 
+    private enum Mode {
+        Time, TimeReset, Date, DateReset;
+    }
+
     private Date theDate;
     private Time theTime;
+    private Mode mode;
+    private String curDisplay;
+
+    public Clock() {
+        theDate = new Date();
+        theTime = new Time();
+        mode = Mode.Time;
+        curDisplay = theTime.showTime();
+    }
 
     public String changeMode() {
-        return null;
+
+        if (mode == Mode.Date) {
+            mode = Mode.Time;
+            curDisplay = theTime.showTime();
+        } else if (mode == Mode.Time){
+            mode = Mode.Date;
+            curDisplay = theDate.showDate();
+        }
+
+        return curDisplay;
+
     }
 
     public String Reset() {
-        return null;
+
+        if (mode == Mode.Date) {
+            mode = Mode.DateReset;
+            curDisplay = "Alter Date";
+        } else  if (mode == Mode.Time) {
+            mode = Mode.TimeReset;
+            curDisplay = "Alter Time";
+        }
+
+        return curDisplay;
     }
 
     public String Set(int p1, int p2, int p3) {
-        return null;
+
+        if (mode == Mode.DateReset) {
+            curDisplay = theDate.setDate(p1, p2, p3);
+        } else if (mode == Mode.TimeReset) {
+            curDisplay = theTime.setTime(p1, p2, p3);
+        }
+
+        return curDisplay;
     }
 
 }
